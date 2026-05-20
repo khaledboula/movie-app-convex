7f7b955171175d7c82819e54621deecba9098edf
khaledboula <kjaledboulesba.dz@gmail.com>
2026-05-20 16:01:58 +0000

    Cleanup: Remove temp files and screenshots

--- extracted_re/components/ui/utils/use-color-scheme.ts
+++
@@ -1,8 +0,0 @@
-import { useColorScheme as useNativeColorScheme } from "react-native";
-
-export function useColorScheme() {
-  const colorScheme = useNativeColorScheme();
-  return colorScheme ?? "light";
-}
-
-export type ColorScheme = "light" | "dark";
